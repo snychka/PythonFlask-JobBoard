c20f47faa67c64d78d989973f3138b0dcfd93bb4
snychka <stefannychka@gmail.com>
2018-09-26 15:43:18 -0600

    module 1

---
+++ jobs/app.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+# http://flask.pocoo.org/docs/1.0/quickstart/#rendering-templates
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
